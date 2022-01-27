10e78d20d81af81c07cd4efbb69533a334cd39c2
riyaz0179508 <riyazahmed2930@gmail.com>
2022-01-27 17:43:19 +0600

    27 jan 2022

--- lib/page_1.dart
+++ lib/page_1.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:ict/four_page.dart';
-import 'package:ict/page_2.dart';
-import 'package:ict/page_3.dart';
-import 'package:ict/page_5.dart';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+const _url = 'https://ictd.gov.bd/';
+const _urlTwo = 'http://www.gameapp.gov.bd/';
+const _urlThree = 'https://bn.wikipedia.org/wiki/%E0%A6%AC%E0%A6%BE%E0%A6%82%E0%A6%B2%E0%A6%BE%E0%A6%A6%E0%A7%87%E0%A6%B6%E0%A7%87%E0%A6%B0_%E0%A6%95%E0%A6%BE%E0%A6%B0%E0%A6%BF%E0%A6%97%E0%A6%B0%E0%A6%BF_%E0%A6%AA%E0%A7%8D%E0%A6%B0%E0%A6%B6%E0%A6%BF%E0%A6%95%E0%A7%8D%E0%A6%B7%E0%A6%A3_%E0%A6%95%E0%A7%87%E0%A6%A8%E0%A7%8D%E0%A6%A6%E0%A7%8D%E0%A6%B0%E0%A7%87%E0%A6%B0_%E0%A6%A4%E0%A6%BE%E0%A6%B2%E0%A6%BF%E0%A6%95%E0%A6%BE';
 
 
 class FirstPage extends StatefulWidget {
@@ -16,22 +17,17 @@ class FirstPage extends StatefulWidget {
 }
 
 class _FirstPageState extends State<FirstPage> {
-var _currentindex=0;
 
 
 
-final pages=[
-  SecondPage(),
-  ThirdPage(),
-  FourPage(),
-  FivePage(),
-];
+
 
   @override
   Widget build(BuildContext context) {
 
     final double screenHeight=MediaQuery.of(context).size.height;
     final double screenWidth=MediaQuery.of(context).size.width;
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -88,129 +84,116 @@ final pages=[
               ),
             ),
 
-            Column(
-              children: [
-                Padding(
-                  padding:  EdgeInsets.only(top: screenHeight*0.22, left: screenWidth*0.02, right: screenWidth*0.02),
-                  child: Container(
-                    height: screenHeight*0.26,
-                    width: screenWidth,
-                    child:
-                    ImageSlideshow(
-                      width: double.infinity,
-                      initialPage: 0,
-                      indicatorColor: Colors.blue,
-                      indicatorBackgroundColor: Colors.grey,
-                      onPageChanged: (value) {
-                        debugPrint('Page changed: $value');
-                      },
-                      autoPlayInterval: 4000,
-                      isLoop: true,
-                      children: [
-                        Image.asset(
-                          'assets/images/aa.jpeg',
-                          fit: BoxFit.fitHeight,
-                        ),
-                        Image.asset(
-                          'assets/images/cc.jpg',
-                          fit: BoxFit.fitHeight,
-                        ),
-                        Image.asset(
-                          'assets/images/dd.jpg',
-                          fit: BoxFit.fitHeight,
-                        ),
-                        Image.asset(
-                          'assets/images/hh.jpg',
-                          fit: BoxFit.fitHeight,
-                        ),
-                        Image.asset(
-                          'assets/images/ii.jpg',
-                          fit: BoxFit.fitHeight,
-                        ),
-                        Image.asset(
-                          'assets/images/kk.jpeg',
-                          fit: BoxFit.fitHeight,
-                        ),
-                      ],
+            SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Padding(
+                    padding:  EdgeInsets.only(top: screenHeight*0.22, left: screenWidth*0.02, right: screenWidth*0.02),
+                    child: Container(
+                      height: screenHeight*0.26,
+                      width: screenWidth,
+                      child:
+                      ImageSlideshow(
+                        width: double.infinity,
+                        initialPage: 0,
+                        indicatorColor: Colors.blue,
+                        indicatorBackgroundColor: Colors.grey,
+                        onPageChanged: (value) {
+                          debugPrint('Page changed: $value');
+                        },
+                        autoPlayInterval: 4000,
+                        isLoop: true,
+                        children: [
+                          Image.asset(
+                            'assets/images/aa.jpeg',
+                            fit: BoxFit.fitHeight,
+                          ),
+                          Image.asset(
+                            'assets/images/cc.jpg',
+                            fit: BoxFit.fitHeight,
+                          ),
+                          Image.asset(
+                            'assets/images/dd.jpg',
+                            fit: BoxFit.fitHeight,
+                          ),
+                          Image.asset(
+                            'assets/images/hh.jpg',
+                            fit: BoxFit.fitHeight,
+                          ),
+                          Image.asset(
+                            'assets/images/ii.jpg',
+                            fit: BoxFit.fitHeight,
+                          ),
+                          Image.asset(
+                            'assets/images/kk.jpeg',
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                
-                SizedBox(height: 30,),
-                
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    children: [
 
-                      ElevatedButton(
-                        
-                          style: ElevatedButton.styleFrom(
-                            padding: EdgeInsets.only(top: 20, bottom: 20, left: 30, right: 30),
-                            primary: Color(0xffa8d7ff),
-                        ),
+                  SizedBox(height: screenHeight*0.10,),
 
-                          onPressed: (){
-
-                          },
-                          child: Text("ICT", style: TextStyle(fontSize: 15, color: Colors.black),)
+                  Padding(
+                    padding:  EdgeInsets.only(left: screenWidth*0.02, right: screenWidth*0.02),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
                       ),
-                    ],
+                        onPressed: _launchURL,
+                        child: Padding(
+                          padding:  EdgeInsets.only(left: screenWidth*0.03, right: screenWidth*0.03, top: screenHeight*0.02, bottom: screenHeight*0.02),
+                          child: Text("তথ্য ও যোগাযোগ প্রযুক্তি বিভাগ", style: TextStyle(fontSize: screenWidth*0.05),),
+                        )),
                   ),
-                )
-              ],
-            ),
+                  SizedBox(height: screenHeight*0.02,),
 
+
+                  Padding(
+                    padding:  EdgeInsets.only(left: screenWidth*0.02, right: screenWidth*0.02),
+                    child: ElevatedButton(
+                        onPressed: launchURL1,
+                        child: Padding(
+                          padding:  EdgeInsets.only(left: screenWidth*0.01, right: screenWidth*0.01, top: screenHeight*0.02, bottom: screenHeight*0.02),
+                          child: Text("কারিগরি প্রশিক্ষণ কেন্দ্রের তালিকা", style: TextStyle(fontSize: screenWidth*0.05)),
+                        )),
+                  ),
+                  SizedBox(height: screenHeight*0.02,),
+                  Padding(
+                    padding: EdgeInsets.only(left: screenWidth*0.02, right: screenWidth*0.02),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.green,
+                        ),
+                        onPressed: launchURL,
+                        child: Padding(
+                          padding:  EdgeInsets.only(left: screenWidth*0.05, right: screenWidth*0.05, top: screenHeight*0.02, bottom: screenHeight*0.02),
+                          child: Text("মোবাইল গেইম ও অ্যাপ্লিকেশন", style: TextStyle(fontSize: screenWidth*0.05)),
+                        )),
+                  ),
+
+
+
+                  SizedBox(height: screenHeight*0.01,),
+
+
+
+                ],
+              ),
+            ),
           ],
         ),
-
-
-
-
-
-
-        bottomNavigationBar: BottomNavigationBar(
-
-          selectedFontSize: 15,
-
-
-
-          currentIndex: _currentindex,
-            items: [
-              BottomNavigationBarItem(
-                 backgroundColor: Colors.lightBlue,
-                  icon: Icon(Icons.home),
-                title: Text("Home"),
-              ),
-
-              BottomNavigationBarItem(
-                icon: Icon(Icons.category),
-                title: Text("Category"),
-
-              ),
-
-              BottomNavigationBarItem(
-                icon: Icon(Icons.wb_incandescent_rounded),
-                title: Text("Training Centre"),
-              ),
-
-              BottomNavigationBarItem(
-                icon: Icon(Icons.more),
-                title: Text("More"),
-              ),
-            ],
-          onTap: (index){
-            setState(() {
-              _currentindex=index;
-            }
-            );
-          },
-        ),
-        //       bottomSheet: pages[_currentindex],
-
-
-
       ),
     );
   }
 }
+void _launchURL() async =>
+    await canLaunch(_url) ? await launch(_url) : throw 'Could not launch $_url';
+
+void launchURL() async =>
+    await canLaunch(_urlTwo) ? await launch(_urlTwo) : throw 'Could not launch $_url';
+
+void launchURL1() async =>
+    await canLaunch(_urlThree) ? await launch(_urlThree) : throw 'Could not launch $_url';
